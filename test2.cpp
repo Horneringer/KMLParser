2c63d6447f11474dbccc0071284d30421bc7d539
Гуреев Вадим Александрович <v_gureev@okb-ipmce.lcl>
2021-10-14 15:48:25 +0300

    Добавил тест для проверки правильности извлечения мощности из названия полигона

--- test2.cpp
+++ test2.cpp
@@ -21,3 +21,19 @@ TEST_CASE("Check parse file name")
 
 	REQUIRE(intelsat.file_name == "Intelsat-10-02-SE-zone");
 }
+
+TEST_CASE("Check parse gain")
+{
+	KMLParser instance4;
+
+	const QString file_path = "02_SE_zone.kml";
+
+	Intelsat intelsat = instance4.zone_coords(file_path);
+
+	QVector<double> gains = {45.5, 43.4, 41.4, 39.4, 37.4};
+
+	for (auto i = 0; i < gains.size(); i++)
+	{
+		REQUIRE(intelsat.zones[i].gain == gains[i]);
+	}
+}
